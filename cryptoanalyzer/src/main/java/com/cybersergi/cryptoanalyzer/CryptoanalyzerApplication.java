290dcb9f12797a374dfaed573d61972d36af7b23
Serhii <serj.sumarenko@gmail.com>
2025-11-24 00:05:57 +0100

    Empty project

---
+++ cryptoanalyzer/src/main/java/com/cybersergi/cryptoanalyzer/CryptoanalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.cybersergi.cryptoanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoanalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoanalyzerApplication.class, args);
+	}
+
+}
---
+++ cryptoanalyzer/src/test/java/com/cybersergi/cryptoanalyzer/CryptoanalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybersergi.cryptoanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoanalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
